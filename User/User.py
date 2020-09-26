c954423675d879067e03ee601f8859ba73808acc
RP-pl <68811106+RP-pl@users.noreply.github.com>
2020-09-26 18:54:59 +0200

    Delete User.py

--- User/User.py
+++
@@ -1,9 +0,0 @@
-from typing import Tuple
-
-
-class User:
-    def __init__(self,login,password):
-        self.password = password
-        self.login = login
-    def getLoginData(self) -> Tuple[str, str]:
-        return self.login, self.password
